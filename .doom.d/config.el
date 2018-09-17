628227a44920e993e2f8854345f78b85a5918891
rohallav <masbervallahor@gmail.com>
2018-09-16 22:01:34 -0300

    some changes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,17 +1,19 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+;; theme
 (setq doom-theme
       ;; 'doom-sourcerer-personal
       'kaolin-fusion
+      kaolin-themes-hl-line-colored t
+      kaolin-themes-underline-wave t
+      kaolin-themes-italic-comments t
+      kaolin-themes-distinct-company-scrollbar t
+      kaolin-themes-distinct-fringe t
       doom-themes-padded-modeline nil
-      doom-treemacs-use-generic-icons nil)
+      doom-treemacs-use-generic-icons nil
+      )
 
 
-;; evil-collection error message
-(setq evil-want-integration t)
-
-(setq-default line-spacing 0.1)
-(add-hook 'magit-revision-mode-hook (lambda () (setq line-spacing 0)))
 ;; fonts
 (setq text-scale-mode-step 1.0
       doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 14 :weight 'medium)
@@ -25,28 +27,27 @@
 ;; default frame
 (add-to-list 'default-frame-alist '(height . 100))
 (add-to-list 'default-frame-alist '(width . 100))
-(set-frame-parameter (selected-frame) 'alpha '(100 . 100))
+;; (set-frame-parameter (selected-frame) 'alpha '(100 . 100))
 
-(setq kaolin-themes-hl-line-colored t)
-(setq kaolin-themes-underline-wave t)
-(setq kaolin-themes-italic-comments t)
+(global-set-key (kbd "C-ç") 'evil-escape)
+
+;; keys
+(setq display-line-numbers-type 'relative)
 (map! :nv "M-F" #'+ivy/project-search
       :gnvime "M-p"  #'projectile-find-file
       :nv "M-s" #'imenu
       :nv "M-S" #'imenu-anywhere
       )
-(add-hook 'rust-mode-hook #'rainbow-delimiters-mode-enable)
-(add-hook 'prog-mode-hook #'+format|enable-on-save)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq display-line-numbers-type 'relative)
 (map! :leader
       :desc "Find file in project" :nv "SPC" #'avy-goto-char-2
       )
+;; modes
 (after! lsp-mode
   (setq lsp-highlight-symbol-at-point nil)
   (setq lsp-eldoc-render-all nil)
   )
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode-enable)
+(add-hook 'prog-mode-hook #'+format|enable-on-save)
 
 (load! "+ranger") ;; File manager stuff
 (load! "+key-chord") ;; Key Chord stuff
